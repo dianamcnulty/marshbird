3c4e3f56f109769a2c38fad87f271869c4c922ed
Diana McNulty <diana.mcnulty@gmail.com>
2023-12-02 13:01:41 -0500

    improve image and card resizing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener  } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
@@ -7,10 +7,27 @@ import {MatGridListModule} from '@angular/material/grid-list';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
+  breakpoint = getBreakPointColumns(window.innerWidth);
   constructor() { }
-
+  @HostListener('window:resize', ['$event'])
   ngOnInit(): void {
+    this.breakpoint = getBreakPointColumns(window.innerWidth)
   }
 
+}
+
+const getBreakPointColumns = (windowWidth) => {
+  let columns
+  switch(true){
+    case windowWidth < 500:
+      columns = 1
+      break;
+    case (windowWidth < 700):
+
+      columns = 3
+      break;
+    default:
+      columns = 4;
+  }
+  return columns
 }
