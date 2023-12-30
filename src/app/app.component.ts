326715d35b856e1435227c776f3731ced2b21ca0
Diana McNulty <diana.mcnulty@gmail.com>
2023-12-29 23:08:45 -0500

    fix errors?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   // title = 'marshbird';
