9ef84fa7363476215a0cee2633fb831343521eee
Diana McNulty <diana.mcnulty@gmail.com>
2023-12-29 22:44:15 -0500

    redirect option

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GameInfoComponent } from './game-info/game-info.component';
 import { LandingComponent } from './landing/landing.component';
+import { RedirectComponent } from './redirect/redirect.component';
 
 const routes: Routes = [
   { path: 'game/:gameid', component: GameInfoComponent},
   { path: 'game/:gameid/:tab', component: GameInfoComponent},
-  { path: '', component: LandingComponent},
+  { path: '', component: RedirectComponent},
   { path: 'defaultsite', redirectTo: ''},
   { path: '**', redirectTo: '' }
 ];
